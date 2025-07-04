d7ea4c42f03c4489ede9c6edf10b5f4cfe9b9374
Sergio Llaneza <sergiollanezalago@gmail.com>
2025-07-04 14:26:32 +0100

    Update download_reference.sh

--- download_reference.sh
+++ download_reference.sh
@@ -12,3 +12,8 @@ wget -nv https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/ge
     ls -lh reference_files
 
 salmon index -t reference_files/gencode.v19.pc_transcripts.fa -i reference_files/salmon_index --gencode -k 7
+
+rm -rf references
+
+mkdir results/fastqc
+mkdir results/fastp
