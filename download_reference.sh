4ccc8f44c1e3802ced60a8cdbf3021e8c7f3c693
Sergio Llaneza <sergiollanezalago@gmail.com>
2025-07-04 14:14:34 +0100

    Update download_reference.sh

--- download_reference.sh
+++ download_reference.sh
@@ -3,12 +3,12 @@
 mkdir -p reference_files && \
     wget -nv https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.pc_transcripts.fa.gz && \
     gunzip gencode.v19.pc_transcripts.fa.gz && \
-    mv gencode.v19.pc_transcripts.fa reference_files/GRCh38_transcript_reference.fa 
+    mv gencode.v19.pc_transcripts.fa reference_files/gencode.v19.pc_transcripts.fa
 
 wget -nv https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz && \
     gunzip gencode.v19.annotation.gtf.gz && \
-    mv gencode.v19.annotation.gtf reference_files/GRCh38_transcript_annotation.gtf && \
+    mv gencode.v19.annotation.gtf reference_files/gencode.v19.annotation.gtf && \
     echo "Files in /reference_files:" && \
     ls -lh reference_files
 
-salmon index -t reference_files/gencode.v19.pc_transcripts.fa -i reference_files/salmon_index --gencode -k 9
+salmon index -t reference_files/gencode.v19.pc_transcripts.fa -i reference_files/salmon_index --gencode -k 7
