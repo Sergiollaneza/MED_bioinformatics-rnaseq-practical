dc05c939b032aa1d923a92415eaf6eb0c3e2ac1e
Sergio Llaneza <sergiollanezalago@gmail.com>
2025-07-04 13:42:11 +0100

    Update download_reference.sh to fix indentation

--- download_reference.sh
+++ download_reference.sh
@@ -5,7 +5,7 @@ mkdir -p reference_files && \
     gunzip gencode.v19.pc_transcripts.fa.gz && \
     mv gencode.v19.pc_transcripts.fa reference_files/GRCh38_transcript_reference.fa 
 
-    wget -nv https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz && \
+wget -nv https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz && \
     gunzip gencode.v19.annotation.gtf.gz && \
     mv gencode.v19.annotation.gtf reference_files/GRCh38_transcript_annotation.gtf && \
     echo "Files in /reference_files:" && \
